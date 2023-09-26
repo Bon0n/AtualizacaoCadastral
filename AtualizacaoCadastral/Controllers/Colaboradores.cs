b144dd9fc4e1fa94b4eca9b52c6302692118dfde
Andrei Bonon <bonon@matrix.local>
2023-09-26 19:43:07 -0300

    Initial

---
+++ AtualizacaoCadastral/Controllers/Colaboradores.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AtualizacaoCadastral.Controllers
+{
+    public class Colaboradores : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult Apagar()
+        {
+            return View();
+        }
+    }
+}
