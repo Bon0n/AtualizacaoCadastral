31af826548a2fbe7388a8fa345b3f5cf312a0023
Andrei Bonão de Oliveira <andrei.oliveira@fitcard.com.br>
2023-09-28 14:19:18 -0300

    Tela de login e logando via AD

--- AtualizacaoCadastral/Controllers/Login.cs
+++ AtualizacaoCadastral/Controllers/Login.cs
@@ -1,10 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AtualizacaoCadastral.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AtualizacaoCadastral.Controllers
 {
     public class Login : Controller
     {
+        public string MensagemLogin { get; private set; }
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Logar(LoginModel login) 
+        {
+            string mensagemLogin = login.Logar();
+            if(mensagemLogin == "Conectado!")
+            {
+                return RedirectToAction("Index", "Colaboradores");
+            }
+            MensagemLogin = mensagemLogin;
+            return RedirectToAction("LoginError", "Login");
+        }
+
+        public IActionResult LoginError() 
         {
             return View();
         }
---
+++ AtualizacaoCadastral/Models/LoginModel.cs
@@ -0,0 +1,42 @@
+﻿using System.DirectoryServices.AccountManagement;
+
+namespace AtualizacaoCadastral.Models
+{
+    public class LoginModel
+    {
+        public string Usuario { get; set; }
+        public string Senha { get; set; }
+        public string MensagemLogin { get; private set; }
+
+        public string Logar()
+        {
+            try
+            {
+                PrincipalContext principalContext = new PrincipalContext
+                (
+                ContextType.Domain,
+                "networksecuritybr.local",
+                "dc=networksecuritybr,dc=local",
+                ContextOptions.Negotiate,
+                Usuario,
+                Senha
+                );
+                if (principalContext.ConnectedServer == "vm-adds-01.networksecuritybr.local" || 
+                    principalContext.ConnectedServer == "vm-adds-02.networksecuritybr.local")
+                {
+                    MensagemLogin = "Conectado!";
+                    return "Conectado!";
+                }
+            } 
+            catch
+            {
+                MensagemLogin = "Credenciais inválidas!";
+
+                return "Credenciais inválidas!";
+            }
+            MensagemLogin = "Não foi possível realizar o login";
+
+            return "Não foi possível realizar o login";
+        }
+    }
+}
