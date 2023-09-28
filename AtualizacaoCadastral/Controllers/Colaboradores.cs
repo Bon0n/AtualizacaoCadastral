a8abdf885ce50f696dfeea25831e4f0e2106cbcd
Andrei Bonão de Oliveira <andrei.oliveira@fitcard.com.br>
2023-09-28 08:53:21 -0300

    Trazendo informações do AD

--- AtualizacaoCadastral/Controllers/Colaboradores.cs
+++ AtualizacaoCadastral/Controllers/Colaboradores.cs
@@ -14,26 +14,91 @@ namespace AtualizacaoCadastral.Controllers
         private PrincipalContext ad = new PrincipalContext(ContextType.Domain);
         public ActionResult Index()
         {
-            var principalContext = AttAdContext.CreatePrincipalContext();
-            var userPrincipal = new UserPrincipal(principalContext);
-
+            PrincipalContext context = new PrincipalContext
+            (
+                ContextType.Domain,
+                "networksecuritybr.local",
+                "dc=networksecuritybr,dc=local",
+                ContextOptions.Negotiate,
+                "services.facilitador",
+                "bed#DONe@Kp!"
+            );
+            var userPrincipal = new UserPrincipal(context);
             var searcher = new PrincipalSearcher(userPrincipal);
+
             var colaboradores = new List<ColaboradorModel>();
-            foreach(var result in searcher.FindAll())
+            ColaboradorModel colaborador;
+            var query = searcher.FindAll();
+            
+            foreach(var result in query)
             {
                 directoryEntry = result.GetUnderlyingObject() as DirectoryEntry;
-                var colaborador = new ColaboradorModel
+                object nome;
+                string usuario = "teste";
+                string email = "teste";
+                string departamento = "teste";
+                object empresa;
+                string cargo = "teste";
+                System.Diagnostics.Debug.WriteLine(directoryEntry.Properties["givenName"].Value);
+
+                nome = directoryEntry.Properties["givenName"].Value;
+                empresa = directoryEntry.Properties["company"].Value;
+
+                /*
+                if (directoryEntry.Properties["givenName"].Value.Equals == null)
+                {
+                    nome = directoryEntry.Properties["givenName"].Value.ToString();
+                }
+                else
+                {
+                    nome = "Nome";
+                }
+                
+                if (!string.IsNullOrWhiteSpace(directoryEntry.Properties["samAccountName"].Value.ToString()))
+                {
+                    usuario = directoryEntry.Properties["samAccountName"].Value.ToString();
+                }
+                else
+                {
+                    usuario = "Usuario";
+                }
+                if (!string.IsNullOrWhiteSpace(directoryEntry.Properties["emailAddress"].Value.ToString()))
+                {
+                    email = directoryEntry.Properties["emailAddress"].Value.ToString();
+                }
+                else
+                {
+                    email = "Email";
+                }
+                if (!string.IsNullOrWhiteSpace(directoryEntry.Properties["department"].Value.ToString()))
+                {
+                    departamento = directoryEntry.Properties["department"].Value.ToString();
+                }
+                else
+                {
+                    departamento = "Departamento";
+                }
+                if (!string.IsNullOrWhiteSpace(directoryEntry.Properties["company"].Value.ToString()))
+                {
+                    empresa = directoryEntry.Properties["company"].Value.ToString();
+                }
+                else
+                {
+                    empresa = "Empresa";
+                }
+                */
+                colaborador = new ColaboradorModel
                 (
-                    directoryEntry.Properties["givenName"].Value.ToString(),
-                    directoryEntry.Properties["samAccountName"].Value.ToString(),
-                    directoryEntry.Properties["emailAddress"].Value.ToString(),
-                    directoryEntry.Properties["department"].Value.ToString(),
-                    directoryEntry.Properties["company"].Value.ToString(),
-                    "Teste"
+                    directoryEntry.Properties["displayName"].Value,
+                    directoryEntry.Properties["emailAddress"].Value,
+                    directoryEntry.Properties["company"].Value,
+                    directoryEntry.Properties["department"].Value,
+                    directoryEntry.Properties["samAccountName"].Value,
+                    ""
                 );
                 colaboradores.Add(colaborador);
             }
-            return View("test",colaboradores);
+            return View(colaboradores);
         }
 
         public IActionResult Criar()
--- AtualizacaoCadastral/Models/ColaboradorModel.cs
+++ AtualizacaoCadastral/Models/ColaboradorModel.cs
@@ -3,22 +3,47 @@
     public class ColaboradorModel
     {
         public int Id { get; private set; }
-        public string Nome { get; private set; }
-        public string Email { get; private set; }
-        public string Empresa { get; private set; }
-        public string Departamento { get; private set; }
-        public string Cargo { get; private set; }
-
+        public object Nome { get; private set; }
+        public string Cpf { get; private set; }
+        public DateTime DataNascimento { get; private set; }
+        public object Empresa { get; private set; }
+        public string Gerente { get; private set; }
         public string EstadoCivil { get; private set; }
+        public string EnderecoCompleto { get; private set; }
+        public string Cor { get; private set; }
+        public string GrauEscolaridade { get; private set; }
+        public string EmailPessoal { get; private set; }
+        public string TelefonePessoal { get; private set; }
+        public string NomeContatoEmergencia { get; private set; }
+        public string TelefoneContatoEmergencia { get; private set; }
+        public string GrauParentescoContatoEmergencia { get; private set; }
+        public string TipoSanguineo { get; private set; }
+        public string Alergias { get; private set; }
+        public string TratamentosSaude { get; private set; }
+        public string UsoMedicamentos { get; private set; }
+        public string NomeDependente { get; private set; }
+        public string GrauParentescoDependente { get; private set; }
+        public string DataNascimentoParentesco { get; private set; }
+        public string CpfParentesco { get; private set; }
+        public object EmailCorporativo { get; private set; }
+        public object Departamento { get; private set; }
+        public object Cargo { get; private set; }
 
-        public ColaboradorModel(string nome, string email, string empresa, string departamento, string cargo, string estadoCivil)
+        
+
+        public ColaboradorModel()
+        {
+            
+        }
+        public ColaboradorModel(object nome, object email, object empresa, object departamento, object cargo, string estadoCivil)
         {
             Nome = nome;
-            Email = email;
+            EmailCorporativo = email;
             Empresa = empresa;
             Departamento = departamento;
             Cargo = cargo;
             EstadoCivil = estadoCivil;
         }
+        
     }
 }
